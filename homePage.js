dcf868d762c723c5738600009391250b26b04166
williamhaynes <w.haynes@rgu.ac.uk>
2016-04-01 09:58:07 +0100

    Initial commit

---
+++ homePage.js
@@ -0,0 +1,42 @@
+$(function(){
+	// 'main' function called when page is loaded and ready
+	
+	// complete the following using jQuery
+	
+	// 1. find the addcontent button in task-two.html
+	// 2. add an event listener for 'click' events
+	// 3. assign the addContent() function to the click event
+	$('#addPepper').click(function () {
+		addContent();	
+
+	});
+	
+	$('#removePepper').click(function(){
+		removeContent();
+	});
+	
+	$('#clearPepper').click(function(){
+		clearContent();
+	});
+});
+
+function addContent () {
+	var itemToAdd = $("#searchBox"); //Get contents of SearchBar
+	items_html = "<li>" + itemToAdd.val() + "</li>"; //Add Wrappers
+	if($("li").size()<12){
+		$('#iAdded ul').append(items_html) //Add li to existing ul
+	}
+	else {
+		removeContent();
+		$('#iAdded ul').append(items_html) //Add li to existing ul
+	}
+	$('#searchBox').val("");
+}
+
+function removeContent(){
+	$('#iAdded li:last-child').remove();
+}
+
+function clearContent(){
+	$("ul").empty();
+}
