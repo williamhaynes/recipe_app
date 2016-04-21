430e1bcc785a5fe94c1ed6779a8a7027509dc444
williamhaynes <w.haynes@rgu.ac.uk>
2016-04-21 11:42:27 +0100

    added autoComplete.js not working yet

---
+++ autoComplete.js
@@ -0,0 +1,45 @@
+/**
+ * Created by William Haynes
+ * 21/04/2016.
+ */
+
+/*
+ * This function is called by the searchBox on the index page
+ * It's calls the Spoonacular APIs autocomplete ingredient function
+ * It's purpose is to make it easier for users who can select from a list as they type.
+ */
+$(function(){
+    $('#searchBox').submit(function () {
+        var searchterm = $("#searchBox").val();
+        //call autocomplete function
+        autocompleteIngredients(searchterm);
+        return false;
+    });
+});
+
+/*
+ *This part of the function actually calls the json data
+ */
+function autocompleteIngredients(searchterm){
+    //call API
+    //build url for request
+    var url = "'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/autocomplete?query=' + searchterm + '-H 'X-Mashape-Key: e3bX6hAPJ4mshViuB0lABloX6jbWp1jr09AjsnW6Ut24xxUcOX'";
+    $.getJSON(url, function(jsondata){
+        //handle results
+        autocompleteText(jsondata);
+    });
+}
+/*
+ * This part of the function returns and populates the drop down list on the search box
+ */
+function autocompleteText(jsondata){
+
+    var htmlstring = "";
+    var counter = 0;
+    var i = 0;
+
+    while(counter<10){ //limits list to first 10 results
+
+        htmlstring +="<option value=\"" +[i]+ "\">" + jsondata.Search[i] + "</option>" + "<br>";
+    }
+}
