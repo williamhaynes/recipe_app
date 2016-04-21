c71eada9dbb3353d598cc446dea8afd677f76020
williamhaynes <w.haynes@rgu.ac.uk>
2016-04-21 12:59:35 +0100

    jsondata should update ul in index.html

--- autoComplete.js
+++ autoComplete.js
@@ -11,7 +11,7 @@
 $(function(){
     var htmlstring = "";
     $('#searchBox').keydown(function() {
-        var grabbedText = ('#searchBox').val();
+        var grabbedText = $('#searchBox').val();
         htmlstring += "<li>"+grabbedText+"</li>";
         $("#dropDown").html(htmlstring);
         //comment
